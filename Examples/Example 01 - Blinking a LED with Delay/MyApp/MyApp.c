8dc7d45adc4a4bb8a90bc318c4c00f1354cfa962
JorgeHSantana <38670645+JorgeHSantana@users.noreply.github.com>
2020-02-25 23:12:31 -0300

    Update MyApp.c

--- Examples/Example 01 - Blinking a LED with Delay/MyApp/MyApp.c
+++ Examples/Example 01 - Blinking a LED with Delay/MyApp/MyApp.c
@@ -5,5 +5,5 @@ void vSetup(void){
 
 void vLoop(void){
 	HAL_GPIO_TogglePin(LED_BUILTIN_GPIO_Port, LED_BUILTIN_Pin);		//Changes LED_BUILTIN state.
-	HAL_Delay(1000);												//One second delay.
+	HAL_Delay(1000);							//One second delay.
 }
